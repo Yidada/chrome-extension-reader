50d5a409776cf13246ad2ff6e52934cb13c3a721
daming zhang <damingzhang@damingdeMacBook-Pro.local>
2018-10-04 12:52:54 +0800

    update

--- background.js
+++ background.js
@@ -3,11 +3,13 @@
  * @author zyd
  */
 
-
 /* eslint-disable no-undef */
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({color: '#3aaa757'}, function () {
+    chrome.storage.sync.set({
+        color: '#3aaa757'
+    }, function () {
         console.log('The color is green.');
     });
 });
+
 /* eslint-enable no-undef */
